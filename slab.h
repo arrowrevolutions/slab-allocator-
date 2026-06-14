a101bb11036ecf3c9080a32cf9493a47f462d41f
arrowrevolutions <219896697+arrowrevolutions@users.noreply.github.com>
2026-06-14 21:52:28 +0500

    Update slab.h

--- slab.h
+++ slab.h
@@ -1,6 +1,6 @@
 /* 
  * Copyright (c) 2026 Arrow Revolutions. All rights reserved.
- * Licensed under the Memory Allocator License (Non-Commercial, Anti-Aggressive Forking).
+ * Licensed under the Memory Allocator License (Memory Allocation License).
  * Project repository: https://github.com/arrowrevolutions/slab-allocator-/tree/main
  */
 #ifndef SLAB_H
